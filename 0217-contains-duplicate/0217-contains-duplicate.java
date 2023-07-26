c5e94c388d95d0bd8f47f1e7abf7c2d98a5b5e7f
Thuy Nhu Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2023-07-26 15:57:52 -0500

    Time: 21 ms (33.53%), Space: 57.7 MB (17.91%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-    Arrays.sort(nums);
+    Arrays.sort(nums); //do cái code này nó bắt cặp lần lươt. kiểu 0-1, 1-2. Mà trong array 1,2,3,1 thì nó kh pair đc 1st lást num --> sort để các số nhỏ lặp đứng gần nhau thì dễ so hơn
     for (int i = 0; i < nums.length - 1; ++i) {
         if (nums[i] == nums[i + 1]) return true;
     }
