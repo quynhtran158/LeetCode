a641db7a2758a5e265ce42b7c37f8d6a3a2a1c26
quynhtran158 <122650930+quynhtran158@users.noreply.github.com>
2023-09-01 16:02:14 -0400

    Time: 64 ms (60.97%), Space: 46.6 MB (16.92%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -3,33 +3,20 @@
  * @param {number[]} nums
  * @return {number}
  */
+var minSubArrayLen = function(target, n) {
+    let l =0;
+    let sum = 0;
+    let ans= Infinity;
 
- //using 2 pointer
-var minSubArrayLen = function(target, nums) {
-    let L = 0;
-    let total = 0;
-    let ans = nums.length;
-/** L for left pointer
-R for right pointer
-total for sum of subarray 
-*/
+    for (r = 0 ; r < n.length; r++){
+        sum += n[r];
+        while (sum >= target){
+            ans = Math.min (ans, r-l+1);
+            sum -= n[l];
+            l++;
 
-let sum = 0;
-for (let i = 0; i < nums.length; i++ ) {
-  sum += nums[i];
-}
-if (nums.length === 0 || sum < target) {
-    return 0;
-}
-for (R = 0; R < nums.length; R++){
-    total += nums[R];
-    if (total >= target)
-        ans = Math.min(ans, R-L+1);
-    while (total - nums[L] >= target && L < R) {
-        total -= nums[L]
-        L += 1
-        ans = Math.min(ans, R-L+1);
+        }
     }
-}
-return ans;
-}; 
+    //ans = infinity means array is empty
+        return ans === Infinity ? 0 : ans;
+};
