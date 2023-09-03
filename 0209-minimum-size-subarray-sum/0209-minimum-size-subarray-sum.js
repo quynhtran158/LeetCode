81b40b8e195c44386489add5a23f606a2b232f31
quynhtran158 <122650930+quynhtran158@users.noreply.github.com>
2023-09-02 21:47:55 -0400

    Time: 59 ms (78.64%), Space: 46.2 MB (48.33%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -1,23 +1,23 @@
 /**
+@param {number[]} nums
  * @param {number} target
- * @param {number[]} nums
+ * 
  * @return {number}
  */
-var minSubArrayLen = function(target, n) {
-    let l =0;
-    let sum = 0;
-    let ans= Infinity;
-    //edge case: sum cua ca array van nho hon target -> dung ans = infinity, neu cuoi cung ans van infinity thi la array empty -> return 0
 
-    for (r = 0 ; r < n.length; r++){
-        sum += n[r];
-        while (sum >= target){
-            ans = Math.min (ans, r-l+1);
-            sum -= n[l];
+// funtction (nums, target) thi bi sai -> tai sao?
+var minSubArrayLen = function (target, nums){
+    let l = 0;
+    var total=0;
+    let res = Infinity;
+
+    for (r = 0; r < nums.length; r++){
+        total += nums[r];
+        while (total >= target){
+            res = Math.min(res, r-l+1);
+            total -= nums[l];
             l++;
-
         }
     }
-    //ans = infinity means array is empty
-        return ans === Infinity ? 0 : ans;
-};
+     return res === Infinity ? 0 : res // if the array is empty, res is infinity so the function will return 0, otherwise, function will return ans
+}
