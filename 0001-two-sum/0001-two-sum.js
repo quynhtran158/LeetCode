344ea61620b015276fc6224762a16148043556cc
quynhtran158 <122650930+quynhtran158@users.noreply.github.com>
2023-09-03 16:36:13 -0400

    Time: 59 ms (77.47%), Space: 43.6 MB (18.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,20 +3,17 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
 
-    const prevMap = new Map(); // val: index
+var twoSum = function(nums, target){
+    const prevMap = new Map();
 
-    for (let i = 0; i < nums.length; i++) {
-      const n = nums[i];
-      const diff = target - n;
+    for (i = 0; i < nums.length; i++){
+        const n = nums[i];
+        let diff = target - n;
 
-      if (prevMap.has(diff)) {
-        return [prevMap.get(diff), i];
-      }
-
-      prevMap.set(n, i);
+        if (prevMap.has(diff)){
+            return [prevMap.get(diff),i]; //[] to create an array with index of the diff
+        }
+    prevMap.set(n,i); //neu ma diff chua xuat hien trong hash ma thi set de bo vo hashmap
     }
- 
-
-};
+}
