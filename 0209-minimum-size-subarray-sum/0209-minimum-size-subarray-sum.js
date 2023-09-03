9dfb84a12255bb7550278de5d67da8dcec2aabef
quynhtran158 <122650930+quynhtran158@users.noreply.github.com>
2023-09-02 21:27:52 -0400

    Time: 50 ms (97.3%), Space: 45.7 MB (84.88%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -7,6 +7,7 @@ var minSubArrayLen = function(target, n) {
     let l =0;
     let sum = 0;
     let ans= Infinity;
+    //edge case: sum cua ca array van nho hon target -> dung ans = infinity, neu cuoi cung ans van infinity thi la array empty -> return 0
 
     for (r = 0 ; r < n.length; r++){
         sum += n[r];
