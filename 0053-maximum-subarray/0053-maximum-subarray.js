b671b9eeb0d3d15b849df8bf282deee817287410
quynhtran158 <122650930+quynhtran158@users.noreply.github.com>
2023-09-08 14:55:39 -0400

    Time: 76 ms (44.02%), Space: 50.3 MB (46.63%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -1,34 +1,22 @@
 /**
- * @param {number[]} nums
- * @return {number}
- */
-
-/**
-Cac edge case:
-- Empty array
-- All negative numbers
-- All 0 in array
-- 1 element in array
- */
-
-
- /**
- What if the array is empty, what do you want me to return?
-  */
+* @param {number[]} nums
+* @return {number}
+*/
 var maxSubArray = function(nums) {
-   let maxSub = nums[0]; //initialize the max sub arr with first element of the array
-   let curSum = 0;
+let curSub = 0;
+let maxSub = nums[0];
 
-   for (i=0; i < nums.length ; i++){
-       // if the curSum is negative, we set the current sum back to 0
-       /** this will also right with the array contains all negative number 
-    because */ 
-       if (curSum < 0){
-           curSum = 0;
-       }
+for ( let i = 0; i < nums.length; i++){
+    if (curSub < 0){
+        curSub = 0;
+    }
+    
+    curSub += nums[i];
+    maxSub = Math.max(maxSub, curSub);
+    
+    }
+    return maxSub;
+    
+    
+}
 
-       curSum += nums[i]; //update the sum by adding more number to the current sum
-       maxSub = Math.max(maxSub, curSum) // here, we will compare the maxSub with the curSum. If curSum is larger than maxSub, we update the maxSub with the curSum
-   }
-   return maxSub; 
-};
