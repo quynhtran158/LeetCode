fdb44b06f5dea893d9bd3accfa504525dc1d4bf7
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2023-09-30 23:46:07 -0400

    Time: 60 ms (99.08%), Space: 54.7 MB (44.4%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -1,15 +1,28 @@
 /**
- * @param {number[]} nums
- * @return {boolean}
- */
-var containsDuplicate = function(nums) {
-      numsSet = new Set();
-    for (const num of nums) {/* Time O(N) */
-        if (numsSet.has(num)) return true;
+* @param {number[]} nums
+* @return {boolean}
+*/
 
-        numsSet.add(num);       /* Space O(N) */
+// //brute force TLE
+// var containsDuplicate = function(nums) {
+//     for (let right = 0; right < nums.length; right){
+//         for (let left = 0; left < right; left++){
+//             if (nums[left]=== nums[right]){
+//                 return true;
+//             }
+//     }
+//     }
+//     return false;
+// };
+
+//hashset
+var containsDuplicate= function(nums){
+    let set = new Set();
+    for ( let x of nums){
+        if (set.has(x)){
+            return true;
+        }
+        set.add(x);
     }
-
     return false;
-
-};
+}
