3fcc4ac862042fa74e196edddeeb70a9a263ebe3
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2023-10-01 19:13:11 -0400

    Time: 116 ms (42.19%), Space: 55 MB (22.43%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -2,24 +2,24 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
+var groupAnagrams = function(strs) {
+    const map = new Map();
+    
+    for (let s of strs){
+         let count = new Array(26).fill(0);
+        for (let c of s){
+            count[c.charCodeAt(0)-'a'.charCodeAt(0)] +=1;
+        }
+    
+    const key = count.join(",");
 
-    var groupAnagrams = function (strs) {
-    let res = {}; //mapping charCount to list of anagrams i.e "1e,1a,1t": ["eat", "tea", "ate"]
-    for(let s of strs) {
-        //To count each char:
-        let count = new Array(26).fill(0); //for a....z
-        //We'll index a to 0 till z to idx 25
-        for(let c of s) {
-            count[c.charCodeAt(0) - "a".charCodeAt(0)] += 1;
+    if (!map.has(key)){
+        map.set(key,[]);
         }
-        let commaSeparatedCount = count.join(",");
-        if(commaSeparatedCount in res) {
-            res[commaSeparatedCount].push(s)
-        } else {
-            res[commaSeparatedCount] = [s];
-        }
-        //console.log("res: ", res);
-        //console.log("Object.values(res): ", Object.values(res))
+
+    map.get(key).push(s);
+    
     }
-    return Object.values(res);
+
+    return Array.from(map.values());
 };
