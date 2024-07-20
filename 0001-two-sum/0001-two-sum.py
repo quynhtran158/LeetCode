d93d5b61f2078566204bfe14d92f5666f6657467
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2024-07-20 17:46:14 -0400

    Time: 50 ms (94.47%), Space: 17.7 MB (24.8%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,17 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map = {}
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in map:
-                return [i, map[complement]]
-            map[nums[i]] = i
+        #using dictionary
+        dict = {}
+        
+        #store the index and val in the dict -> key: val
+        for i, val in enumerate(nums):
+            diff = target - val
+            if diff in dict:
+                return [i, dict[diff]]
+            else: 
+                dict[val] = i
+           
+        return None
+        #find the other number which sum up to the target
+
+        
