f27c16b9ee2e571fee451d8e9ede9fb92686f0e7
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2024-07-20 18:33:39 -0400

    Time: 725 ms (67.07%), Space: 27.5 MB (6.35%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,10 +1,26 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_price = float("inf")
-        max_profit = 0
-        for i in range(len(prices)):
-            if prices[i] < min_price:
-                min_price = prices[i]
-            elif prices[i] - min_price > max_profit:
-                max_profit = prices[i] - min_price
-        return max_profit
+        #buy low, sell high
+        buy = float("inf")
+        profit = 0
+
+        for price in prices:
+            if price < buy:
+                buy = price #if the price decrease, then the profit will always be 0, since there is no update of sell
+            else:  #price >= buy:
+                profit = max(profit, price - buy)
+        return profit  
+
+        #set the first day as a buy
+        # for i in range(len(prices)):
+        #     if i == 0:
+        #         buy = prices[i]
+        #     elif prices[i] <= buy:
+        #         buy = prices[i]
+        #     elif prices[i] > buy:
+        #         profit = max(profit,prices[i] - buy)
+        #     else:
+        #         return 0
+        # return profit
+    
+    
