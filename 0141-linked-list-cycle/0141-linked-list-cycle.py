8d74cdca451c712dfcb76baed8392120f0f4542f
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2024-07-20 01:48:41 -0400

    Time: 40 ms (85.08%), Space: 19.1 MB (49.19%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# class Solution:
+#     def hasCycle(self, head: Optional[ListNode]) -> bool:
+#         count = 0
+#         if head is None:
+#             return None
+        
+#         curr = head
+#         while head and head.next: #LL has at least 2 nodes
+#             if count != pos:
+#                 head = head.next
+#                 count += 1
+#             else:
+#                 while curr.next != head:
+#                     curr = curr.next 
+#                 return True
+#         return False
+
+            
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if head is None:
+            return False
+        
+        curr_head = head
+        tail = head
+        
+        while tail and tail.next: # LL has at least 2 nodes
+            curr_head = curr_head.next
+            tail = tail.next.next
+            
+            if curr_head == tail:
+                return True
+        return False
