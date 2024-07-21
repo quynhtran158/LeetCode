2df5a43d9d3d0cdb318abadcd8d491a0bfe6c630
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2024-07-20 20:01:36 -0400

    Time: 59 ms (49.38%), Space: 17.8 MB (33.76%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,15 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        i = 0
-        valley = prices[0]
-        peak = prices[0]
-        maxprofit = 0
-        while i < len(prices) - 1:
-            while i < len(prices) - 1 and prices[i] >= prices[i + 1]:
-                i += 1
-            valley = prices[i]
-            while i < len(prices) - 1 and prices[i] <= prices[i + 1]:
-                i += 1
-            peak = prices[i]
-            maxprofit += peak - valley
-        return maxprofit
+        #buy low, sell high
+        #if continuosly downward trend dont buy
+        #if see a upward trend, buy then sell it
+        #profit only appears when there is an increase 
+
+        profit = 0
+        for i in range(1, len(prices)): 
+        #we will compare the current with the previous price, bc index 0 has nothing before it so we start at index 1
+            if prices[i] > prices[i-1]:
+            #at this point, an increase happens (5>3, 5-3=2) so we have profit
+                profit += (prices[i] - prices[i-1])
+        return profit
+
