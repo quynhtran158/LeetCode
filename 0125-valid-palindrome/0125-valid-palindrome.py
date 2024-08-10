24680df73ebfbce9d20b2efcfb23a0928095cc9b
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2024-08-10 00:30:15 -0400

    Time: 46 ms (51.25%), Space: 17 MB (80.13%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,18 +1,45 @@
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
-        s = s.replace(" ","") 
-        i, j = 0, len(s) - 1
+    '''
+    understand
+    - what is the maximum length of the string?
+    - can the string be empty? if yes what should i return?
+    - can i assume all english alphabet?
+    - is the string case sensitive?
+    
+    plan:
+    keep track of the front and the back of the string
+    keep track of non-alphanumeric character, if non-alphanumeric char skip, is alpha continue checking
+    if iterating through the array, all character is the same as the front and the back then true, otherwise false
 
-       
-        while i < j:
-            while i < j and not s[i].isalnum(): #not a alphanumeric char then skip it for i
-                i += 1
-            while i < j and not s[j].isalnum(): #not a alphanumeric char then skip it for j
-                j -= 1
-            
-            if s[i] != s[j]:
+    implement:
+    have two pointer to keep track of the front and the back of the string
+    if non-alpha, skip, if is alpha continue checking
+
+
+
+    '''
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s)-1
+        #check if the character is alphabet char
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+        #at this point, both char at L and R are character
+        #check if the current alphabet char, lowercase is the same
+            #same char, moving on
+            if s[left].lower() == s[right].lower():
+                left += 1
+                right -= 1
+            else:
                 return False
-            i += 1
-            j -= 1
-        return True 
+        return True
+               
+            
+            
+
+
+
+
+        
