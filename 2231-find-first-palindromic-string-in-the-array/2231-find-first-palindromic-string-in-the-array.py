520bb123619f112c24475ea53bfd37a320bebcba
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2024-08-22 23:48:28 -0400

    Time: 64 ms (97.26%), Space: 16.5 MB (82.26%) - LeetHub

--- 2231-find-first-palindromic-string-in-the-array/2231-find-first-palindromic-string-in-the-array.py
+++ 2231-find-first-palindromic-string-in-the-array/2231-find-first-palindromic-string-in-the-array.py
@@ -3,18 +3,37 @@
 - check dau cuoi cua 1 phan tu -> set the pointer in the loop so that the pointer can be reset each loop for the new word in the words 
 
 '''
-class Solution:
-    def firstPalindrome(self, words: List[str]) -> str:
-        def isPalindrome(word: str) -> bool:
-            left, right = 0, len(word) - 1
-            while left < right:
-                if word[left] != word[right]:
-                    return False
-                left += 1
-                right -= 1
-            return True
+# class Solution:
+#     def firstPalindrome(self, words: List[str]) -> str:
+#         def isPalindrome(word: str) -> bool:
+#             left, right = 0, len(word) - 1
+#             while left < right:
+#                 if word[left] != word[right]:
+#                     return False
+#                 left += 1
+#                 right -= 1
+#             return True
         
+#         for word in words:
+#             if isPalindrome(word):
+#                 return word
+#         return ""
+
+class Solution:
+    def isPalindrome(self, word:str) -> bool:
+        left, right = 0, len(word)-1
+        while left < right:
+            if word[left] != word[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+       
+    def firstPalindrome(self, words: List[str]) -> str:
         for word in words:
-            if isPalindrome(word):
+            if self.isPalindrome(word):
                 return word
         return ""
+            
+
+    
