f824a5158bb6fc2ffd9f793181219fab40f0ca04
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2024-08-24 02:42:44 -0400

    Time: 78 ms (42.55%), Space: 17.8 MB (76.57%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,19 +1,31 @@
-class Solution:
-    """
-    Input: nums = [0,0,1,1,1,2,2,3,3,4]
-                   0 1 
-                     i j
-Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
-    """
-    def removeDuplicates(self, nums: List[int]) -> int:
-        count = 1  # Initialize count to 1 for the first element
-        i = 0  # Initialize i to 0 to start from the first element
-        for j in range(1, len(nums)):
-            if nums[j] != nums[i]:  # Compare current element with the last unique element
-                i += 1  # Move to the next position for the unique element
-                nums[i] = nums[j]  # Update the position with the current unique element
-                count += 1  # Increment count of unique elements
-        
-        return count  # Return the number of unique elements
+'''
+plan:
+- have 2 pointer next to each other, if both pointer is the same value, move left pointer 1 forward move right pointer forward until we reach the first different number
+- reach different number -> we swap, then increase both pointer
+- if after increasing still different -> sw
 
-            
+
+nums = [0,1,0,1,1,2,2,3,3,4]
+          l
+            r
+nums = [0,1,2,1,0,1,2,3,3,4]
+              l
+                      r
+'''
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        left, right = 0, 0
+        count = 1
+        if not nums:
+            return ""
+        while right < len(nums):
+            if nums[left] == nums[right]:
+                right += 1
+            elif nums[left] != nums[right]:
+                left += 1
+                nums[left] = nums[right]
+                count += 1
+        return count
+
+
+        
