2099258f3374403178c43127d77f8d29d01a819e
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2024-08-24 03:12:41 -0400

    Time: 109 ms (99.14%), Space: 18.2 MB (21.43%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,18 +1,37 @@
+'''
+- is this array sorted? -> dont allow to sort array
+- can this array be empty?
+- if this array is not sorted. is their any time complexity constain that you want me to do? if not i will sort the array
+
+plan:
+have left and right pointers at front and back
+check if left is 0, and right not 0, swap
+
+happy case: 
+nums = [1,3,0,0,12]
+            l
+                r
+nums = [3,0,1,0,3,12]
+          l
+          r
+edge case:
+nums = [0,0,0,0,0]
+
+nums =[o,o,o,o,1]
+
+plan:
+have 2 pointer at the beginning - index 0
+move the right pointer until reach
+'''
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # Index to place the next non-zero element.
-        insertPos = 0
-        
-        # Move non-zero elements to the beginning of nums.
-        for num in nums:
-            if num != 0:
-                nums[insertPos] = num
-                insertPos += 1
-        
-        # Fill the remaining positions with zeroes.
-        while insertPos < len(nums):
-            nums[insertPos] = 0
-            insertPos += 1
+        left = 0
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left] #even though 1st index is not 0, but current right is at 1st ind so nothing happen
+                left += 1
+        return nums
+
