381cbce618d5483424bf7b62fd8e4148404652e2
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2024-08-26 18:00:20 -0400

    Time: 408 ms (87.16%), Space: 34.6 MB (15.74%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,20 +1,11 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        #use hashmap, key as the number, value is their frequency
         map = {}
         for num in nums:
-            if num in map:
-                return True
-            else:
+            if num not in map:
                 map[num] = 1
-        return False
-
-        #use set(), if in set return true -> space O(n)
-        seen = set()
-        nums.sort() #this will set time complexity into O(nlogn) but can save time complexity
-        for i in range(len(nums)):
-            if nums[i] in seen:
-                return True
             else:
-                seen.add(nums[i])
+                map[num] += 1
+                return True
         return False
+        
