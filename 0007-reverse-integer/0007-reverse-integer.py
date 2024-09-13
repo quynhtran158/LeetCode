5a053ced8327f0cf06044c73d65df272c50859ee
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2024-09-13 18:06:13 -0400

    Time: 40 ms (31.1%), Space: 16.5 MB (80.19%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,12 +1,6 @@
 class Solution:
     '''
-    how to check the range
-    2 pointer to reverse the integer first, then check the range -> either return 0 or the reverse valiue
-
-    1, reverse use built- in function
-    2. split the integer into array then use 2 pointer
-
-    be careful of the sign
+    reverse thang int, tinh n%10
     '''
 
     def reverse(self, x: int) -> int:
@@ -18,8 +12,7 @@ class Solution:
         reversed_str = ""
         for i in range(len(x_str) -1, -1, -1): #reverse the string
             reversed_str += x_str[i]
-            reversed_int = int(reversed_str) * sign
-
+        reversed_int = int(reversed_str) * sign
         if -2**31 <= reversed_int and reversed_int <= 2**31-1:
             return reversed_int
         else:
