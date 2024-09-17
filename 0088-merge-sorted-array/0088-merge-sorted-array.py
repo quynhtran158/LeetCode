e4b867cdd6e2d786ae24903c69421a1bdf280d26
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2024-09-17 16:40:22 -0400

    Time: 30 ms (96.79%), Space: 16.5 MB (52%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,43 +1,31 @@
-'''
-plan:
-- arrange the number at the back of the nums1, then after sorted the array then return the reverse nums1
-- have 2 pointer left and right, left to iterating through nums1 array, right interating thourgh nums2 array
-- compare the nums1[left] and nums2[right] then put the number in the back of array nums1
-'''
-
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        Do not return anything, modify nums1 in-place instead.
+        nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+                     l.    
+                           ind.       
+                                                    r
+        do i need to increase the length of the nums1 by myself?
+        can nums1 or nums2 be empty? can both arr be empty?
+
         """
-        left = m-1
-        right = n-1
-        insert_ind = m + n - 1
-        #keep iterating until we reach the start of both array
-        #  no need to check the edge case bc the 2P can handle the empty case
-        # if m == 0 or n == 0:
-        #     if m == 0:
-        #         nums1[insert_ind] = nums1[left]
-        #         left -= 1
-        #     else:
-        #         nums1[insert_ind] = nums2[right]
-        #         right -= 1
-        while left >= 0 and right >= 0:
-            if nums1[left] > nums2[right]:
-                nums1[insert_ind] = nums1[left]
-                left -= 1
-                
+        l, r, index = m -1, n-1, (m+n)-1
+        while  l >= 0 and r >= 0:
+            if nums2[r] >= nums1[l]:
+                nums1[index] = nums2[r]
+                r -= 1
+            #move right down bc we add it to the ind, need to find another one
             else:
-                 nums1[insert_ind] = nums2[right]
-                 right -= 1
-            insert_ind -= 1
+                nums1[index] = nums1[l]
+                l -= 1
+            index -= 1
+           
+        # If there are remaining elements in nums2, copy them into nums1
+        #here, where we visted all of 
+        while r >= 0:
+            nums1[index] = nums2[r]
+            r -= 1
+            index -= 1
+        return nums1
 
-        # Copy remaining elements from nums2 to nums1. Even though nums1 is empty but the length of nums1 is still availble. if nums1 is empty then left is -1 then the main code above wont be execute, so it wil jump directly to the code bellow here:
-        while right >= 0:
-            nums1[insert_ind] = nums2[right]
-            right -= 1
-            insert_ind -= 1
-
-        return nums1   
-
-            
+        
