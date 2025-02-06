5dd23e7d4162c6bc4a4c37f9737a16a820cf7135
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-02-05 19:34:14 -0500

    Time: 32 ms (88.23%), Space: 18 MB (7.13%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -1,32 +1,14 @@
-'''
-understand:
-merge the string w1 and w2 interleaving, the rest of the longer string will be inserted right after the shorter string
-- can i assume we all have lowercase english char?
-- can i assume we will never have empty string in both w1 and w2?
-- what is the maximum lenght of both string?
-- do you want me to merge in-place?
-plan:
-- find the shorter len of the string
-- merge the string interleavingly, the append the exceed string of longer string at the end
-- have a new empty array to store the merged string
-
-'''
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        #new empty array to store merged string
         res = []
-        min_len = min(len(word1), len(word2))
-        
-        #work for case which len w1= w2
-        for i in range(min_len):
-            res += word1[i]
-            res += word2[i]
-        
-        if len(word1) > min_len:
-            res += word1[min_len:] #if len w1 longer, add the rest of the char start from min_len index til the end of the word1
+        i, j = 0, 0
 
-        if len(word2) > min_len:
-            res += word2[min_len:]
-        
+        while i < len(word1) and j < len(word2):
+            res.append(word1[i])
+            res.append(word2[j])
+            i += 1
+            j += 1
+        #the shorter len str was fully added, the longer has remaining char
+        res.append(word1[i::]) #add the rest of w1 from index i
+        res.append(word2[j::]) #add the rest of w2 from index j
         return "".join(res)
-        #join() from ["a", "b"] to [ab]
