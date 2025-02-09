600a12b3084ff7ffcb81153bc834aba8e07b7c80
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-02-08 22:08:11 -0500

    Time: 261 ms (42.41%), Space: 20.1 MB (78.68%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,45 +1,43 @@
+from collections import deque
+from typing import List
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        #to check if the grid is empty
-        if not grid:
-            return 0
-        rows, cols = len(grid), len(grid[0])
-        visit = set()
-        islands = 0
+        num_rows = len(grid)
+        num_cols = len(grid[0])
 
-        def bfs(r, c):
-            q = collections.deque()
-            visit.add((r, c))
-            q.append((r, c))
+        def get_neighbors(coord):
+            res = []
+            row, col = coord
+            delta_row = [-1, 0, 1, 0]
+            delta_col = [0, 1, 0, -1]
+            for i in range(len(delta_row)):
+                r = row + delta_row[i]
+                c = col + delta_col[i]
+                if 0 <= r < num_rows and 0 <= c < num_cols:
+                    res.append((r, c))
+            return res
 
-            while q:
+        def bfs(start):
+            queue = deque([start])
+            r, c = start
+            grid[r][c] = '0'  # mark as visited by changing '1' to '0'
+            while len(queue) > 0:
+                node = queue.popleft()
+                for neighbor in get_neighbors(node):
+                    r, c = neighbor
+                    if grid[r][c] == '0':  # already visited or water
+                        continue
+                    queue.append(neighbor)
+                    grid[r][c] = '0'  # mark as visited
 
-            #why popleft()?
-                # use popleft in queue to make sure that we remove the left most element which is FIFO. Popleft will remove the element from the front of the queue, and make sure that we are exploring th closest unvisited neighbor first before moving to more distant ones 
-            #why do we have to remove the element?
-                # to make sure that the cel (r, c) visited is removed so that we don't re-vist that cell
-
-                row, col = q.popleft()
-                directions = [[1, 0] , [-1, 0], [0, 1], [0, -1]]
-                            # down up right left 
-            # declare the directions array with pairs of nums which are dr and dc (in the for loop below) 
-
-                for dc, dr in directions:
-                    r, c = row + dr, col + dc
-            # if nay check dieu kien xem sau khi dich len xuong trai phai thi cai r hoac c co con nam trong grid hay khong
-                    if (r in range(rows) and 
-                        c in range(cols) and
-                        grid[r][c] == "1" and 
-                        (r, c) not in visit):
-
-                        q.append((r, c))
-                        visit.add((r, c))
- #iterate through each row and column               
-        for r in range(rows):
-            for c in range(cols):
-                # "1 " la string vi de bai initialize string
-                # neu gap phai tat ca so 1 ma chua tung visit (va sau do gap so 0) thi increment num of islands ++ 
-                if grid[r][c] == "1" and (r, c) not in visit:
-                    bfs(r, c)
-                    islands += 1
-        return islands
+        count = 0
+        # bfs starting from each unvisited land cell
+        for r in range(num_rows):
+            for c in range(num_cols):
+                if grid[r][c] == '0':  # skip water cells
+                    continue
+                bfs((r, c))  # start bfs from this land cell
+                count += 1  # bfs would find 1 connected island, increment count
+        
+        return count
