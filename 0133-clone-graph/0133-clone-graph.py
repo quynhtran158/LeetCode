03bc40fb069f304c76125dcebabcd996cda1716e
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-02-09 00:20:40 -0500

    Time: 49 ms (17.86%), Space: 18.4 MB (14.76%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -21,14 +21,13 @@ class Solution:
             clonedNode = {root.val: Node(root.val)}
 
             while q:
-                for _ in range(len(q)):
-                    node = q.popleft()
-                    for neighbor in getNeighbor(node):
-                        if neighbor.val not in clonedNode:
-                            clonedNode[neighbor.val] = Node(neighbor.val)
-                            q.append(neighbor)
-                    # Add the neighbor to the current node's clone's neighbors list
-                        clonedNode[node.val].neighbors.append(clonedNode[neighbor.val])
+                node = q.popleft()
+                for neighbor in getNeighbor(node):
+                    if neighbor.val not in clonedNode:
+                        clonedNode[neighbor.val] = Node(neighbor.val)
+                        q.append(neighbor)
+                # Add the neighbor to the current node's clone's neighbors list
+                    clonedNode[node.val].neighbors.append(clonedNode[neighbor.val])
             return clonedNode[root.val]
         return bfs(node)
                         
