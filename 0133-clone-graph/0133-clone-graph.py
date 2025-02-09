5271365346b652a5d5bc5cde2a0c70d267c9a59e
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-02-09 00:19:00 -0500

    Time: 45 ms (43.93%), Space: 18.3 MB (14.76%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,37 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+from collections import deque
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+    
+        def getNeighbor(node):
+            return node.neighbors
+
+        def bfs(root):
+            q = deque([root])
+            
+            clonedNode = {root.val: Node(root.val)}
+
+            while q:
+                for _ in range(len(q)):
+                    node = q.popleft()
+                    for neighbor in getNeighbor(node):
+                        if neighbor.val not in clonedNode:
+                            clonedNode[neighbor.val] = Node(neighbor.val)
+                            q.append(neighbor)
+                    # Add the neighbor to the current node's clone's neighbors list
+                        clonedNode[node.val].neighbors.append(clonedNode[neighbor.val])
+            return clonedNode[root.val]
+        return bfs(node)
+                        
+
+
+        
