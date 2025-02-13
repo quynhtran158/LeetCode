d189f821dfa6b99b5adf74559ec22cf755f5ebeb
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-02-12 22:30:38 -0500

    Time: 0 ms (100%), Space: 18 MB (48.96%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,28 +1,27 @@
 class Solution:
     '''
-    0 1 2 3 4 5
-    5 0 1 2 3 4
-    if left < right -> no rotation -> check left and left + 1
-    if left > right -> have some rotation => need to find the order turning point to the right of the mid
+   no need to check whether the array is rotated
+    check arr[0] and arr[-1]
+    if arr[0] > arr[-1] -> rotated 
+    compare with arr[-1] as target
 
-    '''
+    [40, 50, 10, 20, 30]
+    l
+                    r
+    r < l => possibility r is the smallest, discard right to find the
+    smallest in front of current r
+    
+    2 scenario < right and > right since all item are unique
+    ''' 
     def findMin(self, nums: List[int]) -> int:
         left, right = 0, len(nums)-1
-
-        
-
-        while left < right:
-            if nums[left] < nums[right]: #sorted array, no rotation:
-                return nums[left]
-            mid = (left + right) // 2
-             #nums[left] > nums[right] -> have rotation, need to find the order turning point in the right of mid
-            if nums[mid] > nums[left]:
-                left = mid + 1
-            elif nums[mid] < nums[left]:  #find the turning point to the left of mid
-                right = mid
+        ans = 0 #if the list is not rotated
+        while left <= right:
+            mid = (left+right)//2
+            if nums[mid] <= nums[-1]:
+                ans = mid
+                right = mid -1 #possibility having smaller in the front
             else:
-                return nums[right]
-        return nums[left]    
-            
-
-
+                left = mid + 1
+        return nums[ans]
+        
