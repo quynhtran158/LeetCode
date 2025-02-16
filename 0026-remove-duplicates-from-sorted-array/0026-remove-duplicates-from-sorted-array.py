fda0ebb41f2b6bf11c2b014f235cdd9465133931
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-02-15 22:11:12 -0500

    Time: 3 ms (48.59%), Space: 19.1 MB (30.29%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,30 +1,29 @@
-'''
-plan:
-- have 2 pointer next to each other, if both pointer is the same value, move left pointer 1 forward move right pointer forward until we reach the first different number
-- reach different number -> we swap, then increase both pointer
-- if after increasing still different -> sw
-
-
-nums = [0,1,0,1,1,2,2,3,3,4]
-          l
-            r
-nums = [0,1,2,1,0,1,2,3,3,4]
-              l
-                      r
-'''
 class Solution:
+    '''
+
+    question:
+- empty array?
+
+case:
+- all duplicate
+- all non dupplicate
+- mixed 
+
+plan:
+- have 2 pointers: left pointer to indicate the index of non-duplicate number
+and right pointer to find the non-duplicate number
+
+[0, 1, 1, 1, 1, 2, 2].
+    l
+                r
+                
+key:
+only update left pointer when find the non-duplicate number (arr[left] != arr[right)
+    '''
     def removeDuplicates(self, nums: List[int]) -> int:
-        left, right = 0, 0
-        count = 1
-        if not nums:
-            return ""
-        while right < len(nums):
-            if nums[left] != nums[right]:
-                left += 1
-                nums[left] = nums[right]
-                count += 1
-            right += 1
-        return count
-
-
-        
+        i = 1
+        for j in range(1, len(nums)):
+            if nums[j] > nums[j-1]:
+                nums[i] = nums[j]
+                i += 1
+        return i
