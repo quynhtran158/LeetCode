4407024e01f97c990d22dada5c70708c4f730b66
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-02-15 22:41:15 -0500

    Time: 0 ms (100%), Space: 17.8 MB (49.38%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
@@ -3,44 +3,26 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-# class Solution:
-#     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-#         # if head is None:
-#         #     return None
-        
-#         current = head
-#         len = 0
-#         curr_len = 0
-#         while current:
-#             current = head.next
-#             len += 1
 
-#             if len % 2 !=0:
-#                 while current and curr_len != (len/2):
-#                     current = head.next
-#                     curr_len += 1
-#                 return current
-#             else:
-#                 while current and curr_len != (len/2)+1:
-#                     current = head.next
-#                     curr_len += 1
-#                 return current
-        
-             
+'''
+what to return when the LL is null?
+we use fast and slow pointer to find the middle of LL
+
+odd
+0 1 2 3 4 
+    s 
+        f
+even
+0 1 2 3 null
+    s
+        f 
+'''
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Step 1: Calculate the length of the linked list
-        current = head
-        length = 0
-        while current:
-            length += 1
-            current = current.next
-
-        # Step 2: Find the middle node
-        current = head
-        mid = length // 2
-        for _ in range(mid):
-            current = current.next
-        
-        return current
-
+        if not head: 
+            return null
+        fast = slow = head
+        while fast and fast.next: 
+            slow = slow.next
+            fast = fast.next.next
+        return slow
