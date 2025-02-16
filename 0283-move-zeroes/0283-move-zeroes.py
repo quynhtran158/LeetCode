5987ea6c6ecec556411491a93e81dfd33dbb881d
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-02-15 23:02:22 -0500

    Time: 3 ms (82.39%), Space: 18.9 MB (48.96%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,38 +1,15 @@
-'''
-- is this array sorted? -> dont allow to sort array
-- can this array be empty?
-- if this array is not sorted. is their any time complexity constain that you want me to do? if not i will sort the array
-
-plan:
-have left and right pointers at front and back
-check if left is 0, and right not 0, swap
-
-happy case: 
-nums = [1,3,0,0,12]
-        l
-        r
-nums = [3,0,1,0,3,12]
-          l
-          r
-edge case:
-nums = [0,0,0,0,0]
-
-nums =[o,o,o,o,1]
-
-plan:
-have 2 pointer at the beginning - index 0
-move the right pointer until reach
-'''
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        left = 0
-        for right in range(len(nums)):
-            # if nums[left] != 0:
-            #     left += 1
-            if nums[right] != 0:
-                nums[left], nums[right] = nums[right], nums[left]
-                left += 1
+        if not nums:
+            return None
+        l = 0
+        for r in range(1,len(nums)):
+            if nums[l] == 0 and nums[r] != 0:
+                nums[l], nums[r] = nums[r], nums[l]
+            if nums[l] != 0:
+                l += 1
         return nums
+        
