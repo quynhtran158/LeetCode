fa857cf1ac5cbabe6fcc90bd454a4640fce82010
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-02-16 01:49:35 -0500

    Time: 32 ms (89.91%), Space: 17.8 MB (48.86%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -1,14 +1,19 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        res = []
-        i, j = 0, 0
+        '''
+        respectively add the letter from s1 and s2
+        reach the end of either the string, add the rest of the string to the result
 
-        while i < len(word1) and j < len(word2):
-            res.append(word1[i])
-            res.append(word2[j])
-            i += 1
-            j += 1
-        #the shorter len str was fully added, the longer has remaining char
-        res.append(word1[i::]) #add the rest of w1 from index i
-        res.append(word2[j::]) #add the rest of w2 from index j
+        '''
+        left, right = 0, 0
+        res = []
+        while left < len(word1) and right < len(word2):
+            res.append(word1[left])
+            res.append(word2[right])
+            left += 1
+            right += 1
+        if len(word1) > len(word2):
+            res.append(word1[left::])
+        else:
+            res.append(word2[right::])
         return "".join(res)
