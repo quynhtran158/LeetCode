6e88bb47175b25efea708b03a333dbcac192154f
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-06-23 23:33:14 -0700

    Time: 0 ms (100%), Space: 17.7 MB (82.75%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -4,11 +4,17 @@ what if nums doesn't contain val?
 can nums not contain val? yes
 what if nums contain all val? what to return? return 0
 
-time: O(n)
-loop thru nums, compare each element, if nums[i] == val, skip, 
-if nums[i] != val, k += 1
+2 pointers:
+nums = [1,2,2,3,6,5] (original)
+nums = [1,3,6,5,6,5] (removed)
+              k
+                 i
+val = 2
 
-return k -> count of element in nums that not equal to val
+k = 0, i = 0, increase k when replace nums[k] with a nums[i] != val, i will loop through len(nums)
+we replace the nums[k] where it different from val, so that different value still there in nums, so we still need to replace that -> this is where nums[k] != val work 
+
+return k -> count of element in nums that not equal to val and nums shouldn't contain val
 '''
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
