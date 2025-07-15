536e5ee76adef4a9a28eed917d272ea3e32084e5
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-07-15 15:00:12 -0700

    Time: 4776 ms (13.92%), Space: 79.5 MB (35.16%) - LeetHub

--- 1142-minimum-knight-moves/1142-minimum-knight-moves.py
+++ 1142-minimum-knight-moves/1142-minimum-knight-moves.py
@@ -2,34 +2,33 @@ from collections import deque
 
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
-        # Directions the knight can move in: 8 possible moves
-        delta_row = [-2, -2, -1, 1, 2, 2, 1, -1]
-        delta_col = [-1, 1, 2, 2, 1, -1, -2, -2]
-        
-        # BFS function to find the minimum steps
-        def bfs(start, target):
-            visited = set()
-            queue = deque([start])
-            visited.add(start)
-            steps = 0
+        def getNeighbor(node):
+            deltaRow = [-2, -2, -1, 1, 2, 2, 1 , -1] 
+            deltaCol = [-1, 1, 2, 2, 1 ,-1, -2, -2] 
+            res = []
+            row, col = node
+            for i in range(len(deltaRow)):
+                nr = row + deltaRow[i]
+                nc = col + deltaCol[i]
+                res.append((nr, nc))
+            return res
+        def bfs(root):
+            q = deque([root])
+            visited = {root}
+            step = 0
             
-            while queue:
-                n = len(queue)
-                for _ in range(n):
-                    node = queue.popleft()
-                    if node == target:
-                        return steps
-                    # Explore all 8 possible knight moves
-                    for i in range(8):
-                        r, c = node
-                        new_r, new_c = r + delta_row[i], c + delta_col[i]
-                        if (new_r, new_c) not in visited:
-                            visited.add((new_r, new_c))
-                            queue.append((new_r, new_c))
-                steps += 1
-            return -1  # unreachable target
-
-        # The BFS starts from (0, 0) and we want to find the shortest path to (x, y)
-        return bfs((0, 0), (x, y))
+            while q:
+                for _ in range(len(q)):
+                    node = q.popleft()
+                    if node == (x,y):
+                        return step
+                    for neighbor in getNeighbor(node):
+                        if neighbor in visited:
+                            continue
+                        q.append(neighbor)
+                        visited.add(neighbor)
+                step += 1
+            return step
+        return bfs((0,0))
 
         
