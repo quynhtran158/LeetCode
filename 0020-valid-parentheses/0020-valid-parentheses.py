30373f2809dcf9e7f7bebebeaebee27f6bbdce0e
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-08-30 11:19:16 -0700

    Time: 3 ms (23.12%), Space: 18 MB (10.57%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,24 +1,48 @@
-class Solution:
-    '''
-    - append only open bracket to the stack, wwhen wwe reach a close bracket, we will check if the current top stack is the matching open with the current close bracket or not
-    - have a hashmap to check if the top of the stack if the match open bracket when we we reach an close bracket
-    - if we find a match close-open, pop the stack (open bracket)
-    - if the stack is empty when we are in the middle of iterating process, we return false since we run out of open bracket
-    - if after iterating the str, the stack is empty -> return true since all match pairs have been found
-    '''
+'''
+correct order -> return false immediately if no matching order 
+empty string?
 
+cases:
+start with closing bracket 
+all open, all close
+empty
+
+
+idea:
+add the opening bracket to stack
+have a hashmap to pair the matching open and close bracket 
+when reach the close bracket, pop the stack has opening bracket, check the hashmap 
+to define whether the close and open bracket match 
+
+at the end if the open bracket stack empty means all pairs are match
+
+s = "[({})]
+        |
+
+stack = "[({
+stack.pop = {
+
+time: O(n) worst case loop thru all s 
+space: O(n) worst case all open bracket
+'''
+class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        mapping = {"(": ")", "{":"}", "[":"]"}
+        #key is the closing since we check the value as opening when we pop stack
+        mapping = {")": "(", "]":"[", "}":"{"}
+
         for c in s:
-            if c in mapping:
+            if c not in mapping: #is opening
                 stack.append(c)
-            else: #is the clos bracket
+            else:
                 if not stack:
-                    return False
-                prev_open = stack.pop()
-                if mapping[prev_open] != c:
-                    return False
-        return not stack
+                    return False #the s starts with close bracket
+                if c in mapping:
+                    prev_open = stack.pop()
+                    if mapping[c] != prev_open:
+                        return False
+        return not stack 
                     
 
+
+        
