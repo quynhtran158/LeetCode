27238228d973e3437c1ec98d60e2eac2133cb61c
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-09-24 02:13:18 -0400

    Time: 195 ms (38.5%), Space: 18.5 MB (15.42%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -4,24 +4,18 @@ class Solution:
         checkFreq = Counter(p) #O(n)
         print("checkFreq",checkFreq)
         windowFreq = Counter(s[0:windowSize]) # O(n)
-        print('winFreq', windowFreq)
         res = []
-        print("after")
 
         if checkFreq == windowFreq:
-            res.append(0) #first anagram
+            res.append(0) #first anagram start index
 
         for right in range(windowSize, len(s)):
-            left = right - windowSize
-            windowFreq[s[left]] -= 1 #we remove previous left val in the new window
-            if windowFreq[s[left]] == 0:
-                del windowFreq[s[left]]
+            left = right - windowSize #old left, old start of window
+            windowFreq[s[left]] -= 1 #we remove previous left val in the new window, this is the old start, here we r sliding to new window so need remove old start (left), 
+            # if windowFreq[s[left]] == 0: #clean the counter check, counter ignore 0 
+            #     del windowFreq[s[left]]
             windowFreq[s[right]] += 1
-            print('winFreq', windowFreq)
-            print("------")
             if checkFreq == windowFreq:
                 res.append(left+1) #left + 1 is the new start idx of current window
-                print("left+1", left + 1)
-                print("------")
                 #left is the old window
         return res
