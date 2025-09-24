a723705ad9773ff5b57eb6cc6b4538f27752aaf3
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-09-24 00:27:18 -0400

    Time: 14 ms (6.9%), Space: 18.2 MB (24.14%) - LeetHub

--- 2468-valid-palindrome-iv/2468-valid-palindrome-iv.py
+++ 2468-valid-palindrome-iv/2468-valid-palindrome-iv.py
@@ -5,22 +5,13 @@ count the mismatch, if mismatch <= 2 return true, false otherwise
 '''
 class Solution:
     def makePalindrome(self, s: str) -> bool:
-        def isPalindrome(i,j):
-            while i < j:
-                if s[i] != s[j]:
-                    return False
-                i += 1
-                j -= 1
-            return True 
-            
         missMatch = 0
         l, r = 0, len(s)-1
         while l < r:
             if s[l] != s[r]:
                 missMatch += 1
-                isPalindrome(l+1, r) or isPalindrome(l, r-1)
             l += 1
             r -= 1
-        return True if missMatch <= 2 else False
+        return missMatch <= 2 
                     
         
