afc0f908c1a96e1055dbbcc1977284cd351d8063
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-09-28 15:50:33 -0400

    Time: 0 ms (100%), Space: 18.2 MB (25.34%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -3,27 +3,27 @@ class Solution:
     states need to track: closeCount, openCount and startInd
     start with an empty string, add ( or ) and continye to do so 
     until the length reach 2*n (n is num or pair parentheses)
-
+       start index: the curretn position of the str being build
     invalid when openCount > n and want to add ) but no matching ()
-
+ 
     '''
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
         path = []
-        def dfs(start, openCount, closeCount):
-            #base case
-            if start == n*2:
-                res.append("".join(path)) #path is string
-                return
-            
-            if openCount < n:
+        def dfs(startIndex, openCount, closeCount):
+            #base case reach the max len of str 
+            if startIndex == 2*n: 
+                res.append("".join(path))
+                return 
+            if openCount < n: #valid check, prune if not valide
                 path.append("(")
-                dfs(start + 1, openCount + 1, closeCount)
-                path.pop() #backtrack
+                dfs(startIndex + 1, openCount + 1, closeCount)
+                path.pop()
             if closeCount < openCount:
                 path.append(")")
-                dfs(start + 1, openCount, closeCount + 1)
-                path.pop() #backtrack
+                dfs(startIndex + 1, openCount , closeCount+ 1)
+                path.pop()
         dfs(0,0,0)
         return res
-            
+
+    
