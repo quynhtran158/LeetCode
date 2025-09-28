77fafa185871cc19fbfe43750c0dc98e231a486d
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-09-28 15:55:53 -0400

    Time: 1 ms (47.38%), Space: 18 MB (59.36%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,40 +1,47 @@
-class Solution:
-    '''
-    - dfs, backtracking 
-    swap the number position, no duplicate number in the permutation
-    have a bool list used to track either the numebr is used or not
-    
-    state space tree
-                            []
-                /.           |.      \
-            1.              2.        3
-         /.  \.          /.  \     /.   \
-        2.     3        1.    3.   1.    2
-       /.       \.   /.       \.   /.     \
-       3.        2.  3.        1
+'''
+are all num seperated as element in array or as a string of number? -> array of number
+integer only or negative? -> int only
+is input are distinct number?
+should the permutaion have duplicate? input [1,2,2,3] will have duplicate
+-> unique permutation only
 
-      1,2,3.   1,3,2  
-    '''
+plan:
+- to generate permutation, pick the char in the unused remaining number in the array
+
+                            []
+                        a/.            \b
+                    [a]                  b]
+      a used,         \ unused b.     a /.  \b used, invalid
+                        [ab].         [ba]
+
+    => [ab], [ba] n! = 2! = 4
+
+- state to track: number used or not
+- know this is solution: len == len(number) means all number are used
+- arrage the remaining used number in the array
+
+used as list of bool -> if used, change false to true 
+
+'''
+class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
-        path =[]
+        path = []
         used = [False] * len(nums)
-
-        def dfs(start):
+        def dfs(start): #start_index is used to keep track of the current level of the state-space tree we are in
             if start == len(nums):
-                res.append(path[:]) #int mutable nen can copy de backtrack k modify
-                return
-            
+                res.append(path[:])
+            #check used number or not, if True (=used) we skip it, move to next number
             for i, num in enumerate(nums):
-                #prunning invalid posibility
                 if used[i]:
                     continue
                 path.append(num)
-                used[i] = True
-                dfs(start+1)
-                path.pop() #backtrack
-                used[i] = False #pop num out so False
+                used[i] = True #alr used and in current path
+                dfs(start + 1)
+                path.pop() #remove the number after find solution
+                used[i] = False #backtrack, so remove the num, change to used
         dfs(0)
         return res
 
-       
+
+        
