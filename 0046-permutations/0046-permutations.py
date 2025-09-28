c07c0200bdf1beb4a0f4d8684fc770f97e3bab50
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-09-28 16:24:53 -0400

    Time: 1 ms (47.38%), Space: 17.9 MB (82.6%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,47 +1,45 @@
 '''
-are all num seperated as element in array or as a string of number? -> array of number
-integer only or negative? -> int only
-is input are distinct number?
-should the permutaion have duplicate? input [1,2,2,3] will have duplicate
--> unique permutation only
+                               []
+                (1,2,3).    (1,2,3).   (1,2,3)
+            [1]               [2].          [3]
+           (2,3)             (1,3).        (1,2)
+        [1,2]. [1,3].      [2,1]. [2,3].  [3,1].  [3,2]
+        (3).     (2).      (3).    (1).     (2).    (1)
+    [1,2,3].    [1,3,2].   [2,1,3]. [2,3,1].  [3,1,1]. [3,2,1]
 
-plan:
-- to generate permutation, pick the char in the unused remaining number in the array
 
-                            []
-                        a/.            \b
-                    [a]                  b]
-      a used,         \ unused b.     a /.  \b used, invalid
-                        [ab].         [ba]
+                             []
+                (1,1.,3).    (1,1.,3).                  (1,1.,3)
+            [1]               [1.].                       [3]
+           (1.,3)             (1,3).                     (1,1.)
+        [1,1.]. [1,3].      [1.,1]. [1.,3].         [3,1].  [3,2]
+        (3).     (2).      (3).    (1).              (2).    (1)
+    [1,1.,3].    [1,3,1.].   [1.,1,3]. [1.,3,1].  [3,1,1]. [3,2,1]
 
-    => [ab], [ba] n! = 2! = 4
-
-- state to track: number used or not
-- know this is solution: len == len(number) means all number are used
-- arrage the remaining used number in the array
-
-used as list of bool -> if used, change false to true 
-
+-startIndex: the index of current numeber using to create permutation from the list
 '''
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
         path = []
         used = [False] * len(nums)
-        def dfs(start): #start_index is used to keep track of the current level of the state-space tree we are in
-            if start == len(nums):
+
+        def dfs(startIndex):
+            #based case
+            if startIndex == len(nums):
                 res.append(path[:])
-            #check used number or not, if True (=used) we skip it, move to next number
+                return 
             for i, num in enumerate(nums):
-                if used[i]:
+                if used[i]: #true, alr used
                     continue
                 path.append(num)
-                used[i] = True #alr used and in current path
-                dfs(start + 1)
-                path.pop() #remove the number after find solution
-                used[i] = False #backtrack, so remove the num, change to used
+                used[i] = True
+                dfs(startIndex+1)
+                path.pop()
+                used[i] = False
         dfs(0)
         return res
+        
 
 
         
