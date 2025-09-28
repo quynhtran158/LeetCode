4eda9143ddda7918d860ce2c11c0ee1494632997
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-09-28 14:29:34 -0400

    Time: 5 ms (31.39%), Space: 17.9 MB (45.7%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,47 +1,40 @@
+'''
+need to keep track of the current index in s (to check current start and the remain)
+
+target = "aaab"
+words = ["a","aa","aaa"]
+-> have dupicate, might check the same index in s multiple time in multiple branch -> use memoization to store the result of the recursive function for each value of start_index.
+
+memo[4] = true/false => all paths lead to dfs(4) = true/false
+
+plan:
+startIndex: current index of s, purpose iterate thru s and check the current char vs remaining char
+memo dict = map key startIndex w/ value result of the recursion call
+
+at each index s will check with words[0], if true, keep checking with next index in s, if wrong, move to the next word in words
+
+As soon as you find one successful segmentation, you stop trying other words at that index.
+'''
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        memo: Dict[int, bool] = {}
+        memo = {}
+        def dfs(startIndex):
+        #base case: at the end of the target s, no remaing char to check with words
 
-        def dfs(start_index):
-            if start_index == len(s):
-                return True
-
-            if start_index in memo:
-                return memo[start_index]
-
+            if startIndex == len(s):
+                return True #reach the end of string
+            if startIndex in memo:
+                return memo[startIndex]
             ans = False
-            for word in wordDict: #if false, keep iterating through word in worDict until have True to break, if end worđDict still false, add memo
-                '''
-                We iterate over words and check if the current substring s[start_index:] starts with word.
-                If word matches, we call dfs(start_index + len(word)) recursively to check if the remaining part of s can be broken.
-                If dfs(start_index + len(word)) returns True, we set ans = True and immediately break out of the loop.
-                If none of the words lead to a successful segmentation, we return False.
-                '''
-                if s[start_index:].startswith(word): #check if the first word match
-                    if dfs(start_index + len(word)): #check the remain part can be build up from worDict
-                    #function children return value to function parent
-                    #dfs(8) call dfs(13). dfs(13) true, return true to dfs(8)
-                    #when do backtrack, at dfs(8) is true (since dfs(13) true, return) we break the loop and update memo. memo[8] = true 
-                        '''
-                    memo[8] store in memo so that next time we backtrack to dfs(8) we dont have to check other word vs the string
-                    3rd Call: dfs(8) (Substring "apple")
-                    Loop over words = ["apple", "pen"]:
-                    Checking "apple" ✅ ("apple".startswith("apple"))
-                    Call dfs(13)
-                    **4th Call: dfs(13) (End of String)**
-                    start_index == len(s), return True.
-                    Backtracking: Where the break Happens
-                    Back to dfs(8)
-                    The recursion returned True → ans = True
-                    Break out of the loop immediately → No need to check "pen" (in worđict))
-                    Back to dfs(5)
-                    The recursion returned True → ans = True
-                    Break out of the loop immediately → No need to check "apple" again.
-                        '''
-                        ans = True
+            for word in wordDict:
+                if s[startIndex:].startswith(word): #true then recursion on the remaining s, with the same word in words
+                    if dfs(startIndex + len(word)):
+                        ans = True #after the recursion return true by base case, go back to prev dfs() set ans = true
                         break
 
-            memo[start_index] = ans
+            memo[startIndex] = ans #outside for looop to store the final result computed by recursion of the curretn start_index after we've tried all possible options.
             return ans
-
         return dfs(0)
+
+               
+        
