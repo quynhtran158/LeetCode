a913e01288316d304f3ea28401685a5f3d4b9f83
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-09-29 16:26:57 -0400

    Time: 48 ms (96.06%), Space: 21.1 MB (20.34%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -14,17 +14,33 @@ If both the nodes p and q are in the left subtree, then continue the search with
 If both step 2 and step 3 are not true, this means we have found the node which is common to node p's and q's subtrees.
 and hence we return this common node as the LCA.
 
+TC: worst case skewed tree (1 line) O(n), O(h) with h is height of the tree
+SP: O(1)
 '''
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if not root:
             return None
-        while root:
-            if root.val < min(p.val, q.val): #root.val < both p and q.val => LCA in right branch 
-                root = root.right
-            elif root.val > max(p.val, q.val):
-                root = root.left
-            else:
-                return root
+        #iterative
+        # while root:
+        #     if root.val < min(p.val, q.val): #root.val < both p and q.val => LCA in right branch 
+        #         root = root.right
+        #     elif root.val > max(p.val, q.val):
+        #         root = root.left
+        #     else:
+        #         return root
+
+        #recursive
         
+        parentVal = root.val
+        pVal = p.val
+        qVal = q.val
+
+        if parentVal > max(pVal, qVal):
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif parentVal < min(pVal, qVal):
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
+                
         
