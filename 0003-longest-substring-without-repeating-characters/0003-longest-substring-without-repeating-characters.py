d9cc42851024c97e079e1e47ccb3b271b9dedb5e
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-09-29 11:47:34 -0400

    Time: 19 ms (52.94%), Space: 17.7 MB (82.11%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,33 +1,15 @@
-'''
-plan: 
-
-have a window, keep slding the window to find the longest none repeating char
-put the visted char in a set, if we find a deupplicate, we cut of the window, until the window doesnt contain the char of right side
-when we find a duplicate, we store the len of that window, then calc the max len later
-
-we want to have left stay at the beginning then adjust left manually, and keep iterating through the array by right pointer so we will use while loop
-s = "abcabcbb"
-      l
-         r
-
-'''
-
-
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        max_len = 0
         window = set()
-        curr_len = 0
-        left = 0
+        left = 0 
+        ans = 0
+        if len(s) == 0:
+            return ans
         for right in range(len(s)):
             while s[right] in window:
                 window.remove(s[left])
-                left += 1
-                curr_len -= 1
+                left += 1 #shrink window by removing s[left] until window valid
             window.add(s[right])
-            curr_len += 1
-            max_len = max(curr_len, max_len)
-
-            
-        return max_len
-                
+            ans = max(ans, right-left+1)
+        return ans
+        
