7a6e1184bd5732004dd69b74c33172fe75575cca
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-10-03 17:44:30 -0400

    Time: 7 ms (56.76%), Space: 19.5 MB (18.74%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,16 +1,7 @@
+from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        map = {}
-        for i in range(len(nums)):
-            if nums[i] not in map:
-                map[nums[i]] = 1
-            else:
-                map[nums[i]] += 1
-        
-        for num, count in map.items():
-            if count > len(nums) // 2:
+        count = Counter(nums)
+        for num, freq in count.items():
+            if freq > (len(nums) //2):
                 return num
-                
-        
-        
-        
