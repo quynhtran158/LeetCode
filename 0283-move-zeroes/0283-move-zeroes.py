b4efaed8f8eb17358806b4a09261427c88eae8e8
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-10-03 17:12:57 -0400

    Time: 3 ms (79.86%), Space: 19 MB (31.54%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,15 +1,38 @@
+'''
+[1,3,12,0,0]
+     l 
+          r
+[1,0,0,0,3,12]
+   l
+         r 
+
+
+write pointer at 0, read pointer iterate thru the array, if read reach non-zero, put non-zero to write, after have all non-zero at front, fill the rest (start from write + 1) will 0
+'''
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        if not nums:
-            return None
-        l = 0
-        for r in range(1,len(nums)):
-            if nums[l] == 0 and nums[r] != 0:
-                nums[l], nums[r] = nums[r], nums[l]
-            if nums[l] != 0:
-                l += 1
-        return nums
+        # if not nums:
+        #     return 0
+        # l = 0
+        # for r in range(1, len(nums)):
+        #     #if nums[l] = 0: swap the number at r with num at l, just swap, we dont have o care if the num at r is or is not 0, the checking will be handle by l pointer
+        #     #only move the l when the number at l != 0, keep iterate thru the array by r
+        #     if nums[l] != 0:
+        #         l += 1
+        # #nums[l] = 0: always swap, to move r fwd
+        #     nums[l], nums[r] = nums[r], nums[l]
+        # return nums
         
+        if not nums:
+            return 0 
+        write = 0
+        for read in range(len(nums)):
+            if nums[read] != 0:
+                nums[write] = nums[read]
+                write += 1
+        for i in range(write, len(nums)):
+            nums[i] = 0
+        return nums
