f1c1927c979a0d11c8512df92360717e6e5fedc7
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-10-02 22:42:35 -0400

    Time: 3 ms (46.62%), Space: 18.8 MB (74.33%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,29 +1,21 @@
-class Solution:
-    '''
-
-    question:
-- empty array?
-
-case:
-- all duplicate
-- all non dupplicate
-- mixed 
-
+'''
+[1,2,3,1,2,3,3,4]
+     l 
+            r
 plan:
-- have 2 pointers: left pointer to indicate the index of non-duplicate number
-and right pointer to find the non-duplicate number
+- have 2 pointers: both at head of array
+- use the right pointer to iterate through the array
+- keep moving the right pointer until we reach the different number 
+- increase left pointer by 1, then update the value at left pointer with value at right pointer if we detact the change/find of new number
 
-[0, 1, 1, 1, 1, 2, 2].
-    l
-                r
-                
-key:
-only update left pointer when find the non-duplicate number (arr[left] != arr[right)
-    '''
+
+
+'''
+class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        i = 1
-        for j in range(1, len(nums)):
-            if nums[j] > nums[j-1]:
-                nums[i] = nums[j]
-                i += 1
-        return i
+        left = 0
+        for right in range(len(nums)):
+            if nums[left] != nums[right]:
+                left += 1
+                nums[left] = nums[right]
+        return left + 1        
