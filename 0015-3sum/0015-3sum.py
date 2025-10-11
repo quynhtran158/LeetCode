5a50bce5714a5a20a4e1084054d961a73f8fced9
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-10-11 16:53:57 -0400

    Time: 431 ms (92.41%), Space: 20.7 MB (36.89%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,18 +1,54 @@
+'''
+plan: 
+-4,-1,-1,0,1,2
+       s
+         l
+            r
+
+
+plan: 
+sort array
+- start at index 0 
+- left = start + 1
+- right at the end of array 
+move left and right to find the triplet that can sum to 0 
+if currSum < 0 move left += 1, currSum > 0, move right += 1
+
+then start with new start
+
+TC: O(n)
+SP: O(1)
+
+'''
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        res = set()
-        for i in range(len(nums)):
-            if nums[i] > 0: 
+        res = []
+        for start in range(len(nums)-2):
+            left = start + 1
+            right = len(nums)-1
+
+            if nums[start] > 0:
                 break
-            l, r = i+1, len(nums) - 1
-            while l < r: 
-                cur_sum = nums[l] + nums[r]
-                if nums[i] + cur_sum == 0: 
-                    res.add((nums[i], nums[l], nums[r]))
-                    l += 1
-                elif nums[i] + cur_sum < 0: 
-                    l += 1
-                else: 
-                    r -= 1
+            #prevent duplpicate for first element
+            if start > 0 and nums[start] == nums[start-1]:
+                continue
+
+            while left < right:
+                currSum = nums[start] + nums[left] + nums[right]
+                if currSum > 0:
+                    right -= 1
+                elif currSum < 0:
+                    left += 1
+                else:
+                    res.append([nums[start], nums[left], nums[right]])
+                    #keep moving left, right to find the next number with same start
+                    left += 1 
+                    right -= 1
+                    while left < right and nums[left] == nums[left-1]:
+                        #if dup, skip the current
+                        left += 1
+                    while left < right and nums[right] == nums[right+1]:
+                        #if dup, skip the curretn
+                        right -= 1
         return res
