37009d1e29d9698049d95cfecaad016f26c2b2c7
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-10-11 16:17:29 -0400

    Time: 12 ms (17.77%), Space: 18.5 MB (37.44%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,26 +1,11 @@
-'''
-all english? yes
-case-insensitive
-puntation, symbol? yes
-has space?
-number? yse
-
-left pointer at head, right pointer at tail of array moving toward each other until l >= right
-skip space, skip non-numeric, keep alphanumeric only
-
-
-
-'''
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        #remove space, non-alphanumeric
         str = "".join(char for char in s if char.isalnum())
-        l, r = 0, len(str)-1
-        while l < r:
-            if str[l].lower() != str[r].lower():
-                return False
-            l += 1
-            r -= 1
+        left, right = 0, len(str)-1
+        while left <= right:
+            if str[left].lower() != str[right].lower():
+                return False 
+            left += 1
+            right -=1 
         return True
-
-        
+    
