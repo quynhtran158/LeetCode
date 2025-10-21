5f189ed867b76e00769a8ced4a4036c2a7f0e249
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-10-21 16:30:44 -0400

    Time: 254 ms (45.93%), Space: 20.3 MB (57.55%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,38 +1,35 @@
+from collections import deque
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         numRow, numCol = len(grid), len(grid[0])
-        def get_neighbor(coord):
-            row, col = coord
-            delta_row = [-1, 0, 1, 0]
-            delta_col = [0, 1, 0 , -1]
+
+        def get_neighbors(coord):
+            r, c = coord
+            directions = [(0, 1), (0, -1), (-1, 0), (1, 0)]
             res = []
-        
-            for i in range(len(delta_col)):
-                neighbor_row = row + delta_row[i]
-                neighbor_col = col + delta_col[i]
-                if 0 <= neighbor_row < numRow and 0 <= neighbor_col < numCol:
-                    res.append((neighbor_row, neighbor_col))
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                # check boundary validity
+                if 0 <= nr < numRow and 0 <= nc < numCol:
+                    res.append((nr, nc))
             return res
-        
+
         def bfs(start):
             q = deque([start])
             r, c = start
-            grid[r][c] = "0" #mark = 0 as visited, change from 1 to 0
+            grid[r][c] = "0"  # mark as visited
             while q:
                 node = q.popleft()
-                for neighbor in get_neighbor(node):
-                    nr, nc = neighbor
-                    if grid[nr][nc] == "0":
-                        continue
-                    q.append(neighbor)
-                    grid[nr][nc] = "0"
+                for nr, nc in get_neighbors(node):
+                    if grid[nr][nc] == "1":  # found land
+                        grid[nr][nc] = "0"  # mark visited
+                        q.append((nr, nc))
+
         count = 0
         for r in range(numRow):
             for c in range(numCol):
-                if grid[r][c] == "0":
-                    continue
-                bfs((r,c))
-                count += 1
+                if grid[r][c] == "1":  # new island
+                    count += 1
+                    bfs((r, c))
         return count
-                    
-        
