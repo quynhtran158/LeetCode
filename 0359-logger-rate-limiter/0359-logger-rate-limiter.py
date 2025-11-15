79621d8115f5a8958c981fc07153cd46b59b111f
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-11-14 19:29:04 -0500

    Time: 2 ms (96.84%), Space: 24.8 MB (38.61%) - LeetHub

--- 0359-logger-rate-limiter/0359-logger-rate-limiter.py
+++ 0359-logger-rate-limiter/0359-logger-rate-limiter.py
@@ -1,18 +1,30 @@
 '''
-- hashMap key:val mess: time
+Q - Nov 14
+
+is the 10s count from the 1st time appear/print or from the lastest time? -> from first time print
+hashmap: key:val - message:earliest avail time (curr time + 10s)
+how to update the time?
+
 
 '''
-
 class Logger:
 
     def __init__(self):
-        self.lastSeen= {}
+        self.mapping = {}
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        if message not in self.lastSeen or timestamp - self.lastSeen[message] >= 10:
-            self.lastSeen[message] = timestamp
+        if message not in self.mapping:
+            self.mapping[message ] = timestamp
             return True
-        return False
+        #time sau so với time trước nếu < 10 thì false
+        else:
+            if timestamp < self.mapping[message] + 10:
+                return False
+            else: 
+                self.mapping[message ] = timestamp
+                return True
+            
+        
 
 
 # Your Logger object will be instantiated and called as such:
