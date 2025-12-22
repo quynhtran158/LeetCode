f4c5fef06294f78cd084eea562ec2f12bc67dbf2
Quynh Tran <122650930+quynhtran158@users.noreply.github.com>
2025-12-22 12:18:23 +0700

    Time: 39 ms (94.48%), Space: 19.3 MB (99.14%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -4,36 +4,15 @@
 #         self.val = x
 #         self.next = None
 
-# class Solution:
-#     def hasCycle(self, head: Optional[ListNode]) -> bool:
-#         count = 0
-#         if head is None:
-#             return None
-        
-#         curr = head
-#         while head and head.next: #LL has at least 2 nodes
-#             if count != pos:
-#                 head = head.next
-#                 count += 1
-#             else:
-#                 while curr.next != head:
-#                     curr = curr.next 
-#                 return True
-#         return False
-
-            
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        if head is None:
-            return False
-        
-        curr_head = head
-        tail = head
-        
-        while tail and tail.next: # LL has at least 2 nodes
-            curr_head = curr_head.next
-            tail = tail.next.next
-            
-            if curr_head == tail:
+        fast, slow = head, head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
                 return True
         return False
+        
+        
